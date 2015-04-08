2b93ffe4e29b7aba3ff657c934332d7840130685
Brett Viren <bv@bnl.gov>
2015-04-08 17:48:38 -0400

    Look for eigen3, optionally, really.

--- find_eigen3.py
+++ find_eigen3.py
@@ -3,5 +3,5 @@ def options(opt):
     
 def configure(cfg):
     # likely need to set environment variable: PKG_CONFIG_PATH
-    cfg.check_cfg(package='eigen3',  uselib_store='EIGEN', args='--cflags --libs')
-    cfg.check(header_name="Eigen/Dense", use='EIGEN')
+    cfg.check_cfg(package='eigen3',  uselib_store='EIGEN', args='--cflags --libs', mandatory=False)
+    cfg.check(header_name="Eigen/Dense", use='EIGEN', mandatory=False)
