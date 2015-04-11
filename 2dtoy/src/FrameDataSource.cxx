b7e031058977425ed9265662f6a260b5aa255585
Xin Qian <last.qx@gmail.com>
2015-04-11 14:18:26 -0400

    finish the toy MC filling frame

--- 2dtoy/src/FrameDataSource.cxx
+++ 2dtoy/src/FrameDataSource.cxx
@@ -25,7 +25,46 @@ int WireCell2dToy::FrameDataSource::jump(int frame_number)
   frame.clear();
   
   //main code to construct a frame
+  // first create many traces and initialize the channel number
+  std::vector <WireCell::Trace> traces(8400);
+  for (int i=0;i!=8400;i++){
+    traces[i].chid = i;
+    traces[i].tbin = 0;
+    traces[i].charge.push_back(0);
+  }
+  // start the simulation of random some points and put in charge
+  int npoint = gRandom->Uniform(5,20);
+  float charge;
 
+  //generate point inside a 1m by 1m region
+  Double_t y_low = -1, y_high = 1;
+  Double_t z_low = 5, z_high = 6;
+
+
+  const WireCell::GeomWire * wire_closest;
+  WireCell::WirePlaneType_t plane;
+
+  for (int i=0;i!=npoint;i++){
+    WireCell::Point p;
+    p.x = -1;
+    p.y = gRandom->Uniform(y_low, y_high);
+    p.z = gRandom->Uniform(z_low, z_high);
+    charge = gRandom->Uniform(5,30);
+    
+    for (int j=0;j!=2;j++){
+      plane = static_cast<WireCell::WirePlaneType_t>(j);
+      wire_closest = gds->closest(p,plane);
+      traces[wire_closest->channel()].charge[0] += charge;
+    }
+  }
+  
+
+  // only save the ones with non-zero charge into a frame
+  for (int i=0;i!=8400;i++){
+    if (traces[i].charge[0] >0){
+      frame.traces.push_back(traces[i]);
+    }
+  }
 
   frame.index = frame_number;
   return frame.index;
