3480492a3e359e38a5e645686ca6fabdcfefb3c0
Brett Viren <brett.viren@gmail.com>
2015-05-10 14:27:21 -0400

    Add support for depending on boost.

--- find_package.py
+++ find_package.py
@@ -27,6 +27,7 @@ def options(opt):
     opt.load('waf_unit_test')
     opt.load('find_root', tooldir=_tooldir)
     opt.load('find_eigen3', tooldir=_tooldir)
+    opt.load('boost', tooldir=_tooldir)
     
 
 def configure(cfg):
@@ -34,6 +35,7 @@ def configure(cfg):
     cfg.load('waf_unit_test')
     cfg.load('find_root', tooldir=_tooldir)
     cfg.load('find_eigen3', tooldir=_tooldir)
+    cfg.load('boost', tooldir=_tooldir)
     pass
 
 def build(bld):
