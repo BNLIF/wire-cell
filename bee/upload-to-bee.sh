d91744f8d9b26535c1511d561d571fe044079b54
Brett Viren <bv@bnl.gov>
2015-12-04 14:06:40 -0500

    Chao's script to upload from command line.  Set BROWSER to launch browser on data set page.

---
+++ bee/upload-to-bee.sh
@@ -0,0 +1,22 @@
+LOGIN_URL=http://www.phy.bnl.gov/wire-cell/bee
+COOKIES=cookies.txt
+CURL_BIN="curl -s -c $COOKIES -b $COOKIES -e $LOGIN_URL"
+
+$CURL_BIN $LOGIN_URL > /dev/null
+echo -n "Django Auth: get csrftoken ... "
+DJANGO_TOKEN="$(grep csrftoken $COOKIES | sed 's/^.*csrftoken\s*//')"
+echo $DJANGO_TOKEN
+
+FILENAME=$1
+echo "uploading file $FILENAME ... "
+UUID="$($CURL_BIN \
+    -H "X-CSRFToken: $DJANGO_TOKEN" \
+    -F "file=@$FILENAME" \
+    $LOGIN_URL/upload/)"
+
+# echo "$DJANGO_TOKEN&file=@/home/vagrant/Sites/bee/test/test.zip"
+# echo "Please redirect your browser to:"
+URL="$LOGIN_URL/set/$UUID/event/list/"
+${BROWSER:-echo} "$URL"
+
+rm $COOKIES
