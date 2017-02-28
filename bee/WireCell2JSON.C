b869561d0f425f2e18ac55ae63dc5cd0b59786e8
Xin Qian <last.qx@gmail.com>
2017-02-28 17:06:00 -0500

    fix a bug

--- bee/WireCell2JSON.C
+++ bee/WireCell2JSON.C
@@ -1,7 +1,9 @@
+#include "WCReader.cc"
+
 void WireCell2JSON(TString filename, TString option = "truth", TString outfile="wc.json")
 {
-    gROOT->Reset();
-    gROOT->ProcessLine(".x loadClasses.C" );
+  //gROOT->Reset();
+  // gROOT->ProcessLine(".x loadClasses.C" );
     gErrorIgnoreLevel=2001;
     WCReader r(filename, outfile);
     if (option == "mc") {
