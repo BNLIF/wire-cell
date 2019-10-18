984d130769e2b21c95de5a8af2168c01324c4238
Xin Qian <last.qx@gmail.com>
2019-10-18 14:52:09 -0400

    add an application

---
+++ uboone_nusel_app/apps/count_entries.cxx
@@ -0,0 +1,18 @@
+#include "TFile.h"
+#include "TTree.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2){
+    cerr << "usage: wire-cell-uboone /path/to/match.root" << endl;
+    return 1;
+  }
+  const char* root_file = argv[1];
+  TFile *file1 = new TFile(root_file);
+  TTree *T = (TTree*)file1->Get("Trun");
+  std::cout << T->GetEntries() << std::endl;
+}
