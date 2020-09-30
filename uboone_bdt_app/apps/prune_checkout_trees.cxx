10249af92f26ea76fc83e62859c5ff9b7c3e3f1e
Wenqiang Gu <wgu@bnl.gov>
2020-09-30 01:43:44 -0500

    add script for splitting checkout and weight files

---
+++ uboone_bdt_app/apps/prune_checkout_trees.cxx
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TMath.h"
+
+using namespace std;
+//using namespace LEEana;
+
+int main( int argc, char** argv )
+{
+  if (argc < 2) {
+    std::cout << "prune_checkout_trees #inputROOT #outputROOT" << std::endl;
+    return -1;
+  }
+
+  string filelist(argv[1]);
+  string outName(argv[2]);
+
+  auto T_pot = new TChain("wcpselection/T_pot");
+  auto T_eval = new TChain("wcpselection/T_eval");
+  auto T_PFeval = new TChain("wcpselection/T_PFeval");
+  auto T_BDTvars = new TChain("wcpselection/T_BDTvars");
+  auto T_KINEvars = new TChain("wcpselection/T_KINEvars");
+
+
+  std::ifstream in(filelist.c_str());
+  std::string line;
+  while (std::getline(in, line)) {
+    T_pot->Add(line.c_str());
+    T_eval->Add(line.c_str());
+    T_PFeval->Add(line.c_str());
+    T_BDTvars->Add(line.c_str());
+    T_KINEvars->Add(line.c_str());
+  }
+
+  cout << T_pot->GetEntries() << endl;
+  cout << T_eval->GetEntries() << endl;
+  cout << T_PFeval->GetEntries() << endl;
+  cout << T_BDTvars->GetEntries() << endl;
+  cout << T_KINEvars->GetEntries() << endl;
+
+  auto ofile = new TFile(outName.c_str(), "recreate");
+  ofile->mkdir("wcpselection")->cd();
+  T_pot->Merge(ofile, 0, "keep");
+  T_eval->Merge(ofile, 0, "keep");
+  T_PFeval->Merge(ofile, 0, "keep");
+  T_BDTvars->Merge(ofile, 0, "keep");
+  T_KINEvars->Merge(ofile, 0, "keep");
+  ofile->Close();
+
+  return 0;
+}
---
+++ uboone_bdt_app/apps/prune_weight_trees.cxx
@@ -0,0 +1,204 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TMath.h"
+
+using namespace std;
+//using namespace LEEana;
+
+int main( int argc, char** argv )
+{
+  if (argc < 3) {
+    std::cout << "Usage: prune_weight_trees #txtInputList #outputROOT #GenieFluxKnob" << std::endl;
+    std::cout << "Optional GenieFluxKnob:" << std::endl;
+    std::cout << " 1 UBGenieFluxSmallUni" << std::endl;
+    std::cout << "     All_UBGenie" << std::endl;
+    std::cout << "     AxFFCCQEshape_UBGenie" << std::endl;
+    std::cout << "     DecayAngMEC_UBGenie" << std::endl;
+    std::cout << "     NormCCCOH_UBGenie" << std::endl;
+    std::cout << "     NormNCCOH_UBGenie" << std::endl;
+    std::cout << "     RPA_CCQE_Reduced_UBGenie" << std::endl;
+    std::cout << "     RPA_CCQE_UBGenie" << std::endl;
+    std::cout << "     RootinoFix_UBGenie" << std::endl;
+    std::cout << "     ThetaDelta2NRad_UBGenie" << std::endl;
+    std::cout << "     Theta_Delta2Npi_UBGenie" << std::endl;
+    std::cout << "     TunedCentralValue_UBGenie" << std::endl;
+    std::cout << "     VecFFCCQEshape_UBGenie" << std::endl;
+    std::cout << "     XSecShape_CCMEC_UBGenie" << std::endl;
+    std::cout << "     splines_general_Spline" << std::endl;
+    std::cout << "     xsr_scc_Fa3_SCC" << std::endl;
+    std::cout << "     xsr_scc_Fv3_SCC" << std::endl;
+    std::cout << " 2 expskin_FluxUnisim" << std::endl;
+    std::cout << " 3 horncurrent_FluxUnisim" << std::endl;
+    std::cout << " 4 kminus_PrimaryHadronNormalization" << std::endl;
+    std::cout << " 5 kplus_PrimaryHadronFeynmanScaling" << std::endl;
+    std::cout << " 6 kzero_PrimaryHadronSanfordWang" << std::endl;
+    std::cout << " 7 nucleoninexsec_FluxUnisim" << std::endl;
+    std::cout << " 8 nucleonqexsec_FluxUnisim" << std::endl;
+    std::cout << " 9 nucleontotxsec_FluxUnisim" << std::endl;
+    std::cout << "10 piminus_PrimaryHadronSWCentralSplineVariation" << std::endl;
+    std::cout << "11 pioninexsec_FluxUnisim" << std::endl;
+    std::cout << "12 pionqexsec_FluxUnisim" << std::endl;
+    std::cout << "13 piontotxsec_FluxUnisim" << std::endl;
+    std::cout << "14 piplus_PrimaryHadronSWCentralSplineVariation" << std::endl;
+
+    return -1;
+  }
+
+  string filelist(argv[1]);
+  string outName(argv[2]);
+  string SaveGenieFluxKnob(argv[3]);
+
+  auto T_wgt = new TChain("wcpweights/T_wgt");
+
+  std::ifstream in(filelist.c_str());
+  std::string line;
+  while (std::getline(in, line)) {
+    T_wgt->Add(line.c_str());
+  }
+  // cout << T_wgt->GetEntries() << endl;
+
+  Int_t run, subrun, event;   
+  std::string* file_type = new std::string;
+  Float_t weight_cv, weight_spline, weight_lee;
+  auto mcweight = new std::map<std::string, std::vector<float>>;
+  bool mcweight_filled;
+
+  T_wgt->SetBranchAddress("run", &run);
+  T_wgt->SetBranchAddress("subrun", &subrun);
+  T_wgt->SetBranchAddress("event", &event);
+  T_wgt->SetBranchAddress("file_type", &file_type);
+  T_wgt->SetBranchAddress("weight_cv", &weight_cv);
+  T_wgt->SetBranchAddress("weight_spline", &weight_spline);
+  T_wgt->SetBranchAddress("weight_lee", &weight_lee);
+  T_wgt->SetBranchAddress("mcweight", &mcweight);
+  T_wgt->SetBranchAddress("mcweight_filled", &mcweight_filled);
+  // T_wgt->Print();
+
+
+  std::vector<std::string> UBGenieFluxSmallUni = {
+    "All_UBGenie"
+    , "AxFFCCQEshape_UBGenie"
+    , "DecayAngMEC_UBGenie"
+    , "NormCCCOH_UBGenie"
+    , "NormNCCOH_UBGenie"
+    , "RPA_CCQE_Reduced_UBGenie"
+    , "RPA_CCQE_UBGenie"
+    , "RootinoFix_UBGenie"
+    , "ThetaDelta2NRad_UBGenie"
+    , "Theta_Delta2Npi_UBGenie"
+    , "TunedCentralValue_UBGenie"
+    , "VecFFCCQEshape_UBGenie"
+    , "XSecShape_CCMEC_UBGenie"
+    , "splines_general_Spline"
+    , "xsr_scc_Fa3_SCC"
+    , "xsr_scc_Fv3_SCC"
+  };
+
+  std::vector<std::string> UBGenieFluxBigUni = {
+    "expskin_FluxUnisim"
+    , "horncurrent_FluxUnisim"
+    , "kminus_PrimaryHadronNormalization"
+    , "kplus_PrimaryHadronFeynmanScaling"
+    , "kzero_PrimaryHadronSanfordWang"
+    , "nucleoninexsec_FluxUnisim"
+    , "nucleonqexsec_FluxUnisim"
+    , "nucleontotxsec_FluxUnisim"
+    , "piminus_PrimaryHadronSWCentralSplineVariation"
+    , "pioninexsec_FluxUnisim"
+    , "pionqexsec_FluxUnisim"
+    , "piontotxsec_FluxUnisim"
+    , "piplus_PrimaryHadronSWCentralSplineVariation"
+  };
+
+  auto ofile = TFile::Open(outName.c_str(), "recreate");
+  // ofile->mkdir("wcpselection")->cd();
+
+  // set branch
+  TTree* UBTree = nullptr;
+  std::map<std::string, std::vector<float> > weight_f1;
+  std::vector<float> weight_f2;
+
+  if (SaveGenieFluxKnob == "UBGenieFluxSmallUni") {
+    UBTree = new TTree("UBGenieFluxSmallUni","UBGenieFluxSmallUni");
+    UBTree->Branch("run", &run, "run/I");
+    UBTree->Branch("subrun", &subrun, "subrun/I");
+    UBTree->Branch("event", &event, "event/I");
+    UBTree->Branch("file_type", &file_type);
+    UBTree->Branch("weight_cv", &weight_cv, "weight_cv/F");
+    UBTree->Branch("weight_spline", &weight_spline, "weight_spline/F");
+    UBTree->Branch("weight_lee", &weight_lee, "weight_lee/F");
+    UBTree->Branch("mcweight_filled", &mcweight_filled);
+
+    for (auto const& knob : UBGenieFluxSmallUni) {
+      weight_f1.emplace(knob, std::vector<float>());
+      UBTree->Branch(knob.c_str(), &(weight_f1.at(knob)) );
+    }
+
+  }
+  else {
+    int N = std::count(UBGenieFluxBigUni.begin(), UBGenieFluxBigUni.end(), SaveGenieFluxKnob);
+    if (N!=1) {
+      std::cerr << "Warning, cannot find the knob: " << SaveGenieFluxKnob << std::endl;
+      return 0;
+    }
+
+    UBTree = new TTree(SaveGenieFluxKnob.c_str(), SaveGenieFluxKnob.c_str());
+    UBTree->Branch("run", &run, "run/I");
+    UBTree->Branch("subrun", &subrun, "subrun/I");
+    UBTree->Branch("event", &event, "event/I");
+    UBTree->Branch("file_type", &file_type);
+    UBTree->Branch("weight_cv", &weight_cv, "weight_cv/F");
+    UBTree->Branch("weight_spline", &weight_spline, "weight_spline/F");
+    UBTree->Branch("weight_lee", &weight_lee, "weight_lee/F");
+    UBTree->Branch("mcweight_filled", &mcweight_filled);
+    UBTree->Branch(SaveGenieFluxKnob.c_str(), &weight_f2);
+  }
+
+
+  // loop through events
+  for (size_t i=0; i<T_wgt->GetEntries(); i++) {
+    T_wgt->GetEntry(i);
+    // for (auto const& ele: *mcweight) {
+    //   cout << ele.first << " " << ele.second.size() << endl;
+    // }
+
+    for (auto x: weight_f1) { x.second.clear(); }
+    weight_f2.clear();
+ 
+    if (mcweight_filled>0) {
+      // cout << mcweight_filled << endl;
+      if (SaveGenieFluxKnob=="UBGenieFluxSmallUni") {
+        for(auto const& knob: UBGenieFluxSmallUni) {
+          // weight_f1->push_back(mcweight->at(knob));
+          weight_f1.at(knob) = mcweight->at(knob);
+        }
+      }
+      else {
+         weight_f2 = mcweight->at(SaveGenieFluxKnob);
+      }
+    }
+ 
+    UBTree->Fill();
+  }
+
+  UBTree->Write();
+  ofile->Close();
+
+ 
+  
+ 
+  return 0;
+}
