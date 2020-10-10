35158a5bbf1a80cf96356cdd17894bd82811a019
Xin Qian <last.qx@gmail.com>
2020-10-10 11:46:41 -0400

    recover code

--- uboone_bdt_app/src/master_cov_matrix.cxx
+++ uboone_bdt_app/src/master_cov_matrix.cxx
@@ -487,8 +487,10 @@ void LEEana::CovMatrix::gen_det_cov_matrix(int run, std::map<int, TH1F*>& map_co
     TMatrixD matrix_variation = matrix_eigenvector * matrix_element;
     double rel_err = random3.Gaus(0,1);
     for (int j=0;j!=rows;j++){
-      matrix_variation(j,0) += (*vec_mean)(j);
+      matrix_variation(j,0) += (*vec_mean_diff)(j); // standard ...
+      //matrix_variation(j,0) = (*vec_mean_diff)(j); // no random term
       x[j] = rel_err * matrix_variation(j,0);
+      //x[j] = matrix_variation(j,0); //original no abs term
     }
     prin_full.AddRow(x);
     
